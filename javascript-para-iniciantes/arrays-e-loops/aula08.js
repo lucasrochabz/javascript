4f9c93fb247d60f90f43d5f9c5b82810145be33f
Lucas Rocha <lucasbezerrar@gmail.com>
2024-01-18 16:03:43 -0300

    Add aulas Atribuição e Ternário e Escopo

--- javascript-para-iniciantes/arrays-e-loops/aula08.js
+++ javascript-para-iniciantes/arrays-e-loops/aula08.js
@@ -1,3 +1,24 @@
-var nomes = ['Lucas', 'Bezerra', 'Rocha']
+// Exercício 8
 
-var ultimoItem = nomes.pop()
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var titulos = [1959, 1962, 1970, 1994, 2002]
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O Brasil ganhou a copa de ${ano}`
+for(var i = 0; i < titulos.length; i++) {
+  console.log(`O Brasil ganhou a copa de ${titulos[i]}.`)
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+for(var fruta = 0; fruta < frutas.length; fruta++) {
+  console.log(frutas[fruta])
+  if (frutas[fruta] === 'Pera')
+  break
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length - 1]
---
+++ javascript-para-iniciantes/escopo/aula10.js
@@ -0,0 +1,33 @@
+// Exercício 10
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas)
+}
+/* forma errada
+console.log(var, marca, portas);
+*/
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
