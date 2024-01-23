5768dff1a9972c90af4ec38dc448fe38f30ba0f8
Lucas Rocha <lucasbezerrar@gmail.com>
2024-01-23 16:50:08 -0300

    Add novas aulas

---
+++ dom-para-iniciantes/selecao-de-elementos/script.js
@@ -0,0 +1,26 @@
+// Exercício
+
+// Retorne no console todas as imagens do site
+const img = document.querySelectorAll('img')
+console.log(img)
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]')
+console.log(imagensAnimais)
+
+// Selecione todos os links internos (onde o href começa com #)
+const linksInterno = document.querySelectorAll('[href^="#"]')
+console.log(linksInterno)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const h2Animais = document.querySelector('.animais-descricao h2')
+console.log(h2Animais)
+
+const animais = document.querySelector('.animais-descricao')
+const h2AnimaisTeste = animais.querySelector('h2')
+console.log(h2AnimaisTeste)
+
+// Selecione o último p do site
+const paragrafos = document.querySelectorAll('p')
+console.log(paragrafos[paragrafos.length - 1])
+console.log(paragrafos[--paragrafos.length])
