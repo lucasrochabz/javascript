1e6dcfa7e482ecf51aa1bc172d231ae67b269a43
Lucas Rocha <lucasbezerrar@gmail.com>
2024-03-06 16:22:35 -0300

    Add aulas de Array, Function e Object

--- objetos/array/objetos06.js
+++ objetos/array/objetos06.js
@@ -1,1 +1,50 @@
-console.log('obj 6')
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
+
+const primeiroValor = comidas.shift()
+console.log(primeiroValor)
+
+const ultimoValor = comidas.pop()
+console.log(ultimoValor)
+
+const addArroz = comidas.push('Arroz')
+console.log(comidas)
+
+const addPeixeEBatata = comidas.unshift('Peixe', 'Batata')
+console.log(comidas)
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
+
+console.log(estudantes.sort())
+console.log(estudantes.reverse())
+console.log(estudantes.includes('Joana'))
+console.log(estudantes.includes('Juliana'))
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+
+html = html.split('section').join('ul').split('div').join('li')
+console.log(html)
+
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+
+const carrosCopia = carros.slice()
+carros.pop()
+
+console.log(carros)
+console.log(carrosCopia)
