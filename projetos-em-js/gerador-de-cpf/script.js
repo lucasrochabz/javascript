15232efd07a33e518df1f06177f0672867236437
Lucas Rocha <lucasbezerrar@gmail.com>
2024-03-28 16:12:53 -0300

    Add projeto Gerador de CPF

---
+++ projetos-em-js/gerador-de-cpf/script.js
@@ -0,0 +1,42 @@
+const cpfElement = document.querySelector('#cpf');
+const gerarCpfBtn = document.querySelector('#gerar-cpf');
+const copiarCpfBtn = document.querySelector('#copiar-cpf');
+
+
+function gerarCpf() {
+  let n = Math.floor(Math.random() * 999999999) + 1;
+  let nStr = n.toString().padStart(9, '0');
+  let dv1 = calcularDV(nStr, 10);
+  let dv2 = calcularDV(nStr + dv1, 11);
+  
+  cpfElement.innerText = formatarCpf(nStr + dv1 + dv2);
+}
+
+function calcularDV(numero, peso) {
+  let total = 0;
+  for (let i = 0; i < numero.length; i++) {
+    total += parseInt(numero.charAt(i)) * peso --;
+  }
+  let resto = total % 11;
+  return resto < 2 ? 0 : 11 - resto
+}
+
+function formatarCpf(cpf) {
+  const regex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
+  return cpf.replace(regex, '$1.$2.$3-$4')
+}
+
+function copiarCpf() {
+  const cpf = cpfElement.innerText
+  navigator.clipboard.writeText(cpf).then(
+    () => {
+      alert(`CPF ${cpf} copiado para a área de transferência.`)
+    },
+    (err) => {
+      console.log('Erro ao copiar CPF.')
+    }
+  );
+}
+
+gerarCpfBtn.addEventListener('click', gerarCpf);
+copiarCpfBtn.addEventListener('click', copiarCpf);
